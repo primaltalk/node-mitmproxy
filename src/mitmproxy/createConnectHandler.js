6d78a0ec78c4c6b897d5e082dd957132014f9eb6
Ian Williams <ian.williams@primaltalk.com>
2020-03-13 10:52:41 +0000

    Code cleanup.

--- src/mitmproxy/createConnectHandler.js
+++ src/mitmproxy/createConnectHandler.js
@@ -1,50 +1,49 @@
-const net = require('net');
-const url = require('url');
-const colors = require('colors');
-const connections = require('../common/connections');
+const net = require("net");
+const url = require("url");
+const colors = require("colors");
+const connections = require("../common/connections");
 
-const localIP = '127.0.0.1';
+const localIP = "127.0.0.1";
 
 // create connectHandler function
 module.exports = function createConnectHandler(sslConnectInterceptor, fakeServerCenter) {
-
   // return
-    return function connectHandler (req, cltSocket, head) {
+  return function connectHandler(req, cltSocket, head) {
+    var srvUrl = url.parse(`https://${req.url}`);
 
-        var srvUrl = url.parse(`https://${req.url}`);
-
-        if (typeof sslConnectInterceptor === 'function' && sslConnectInterceptor.call(null, req, cltSocket, head)) {
-            fakeServerCenter.getServerPromise(srvUrl.hostname, srvUrl.port).then((serverObj) => {
-                connect(req, cltSocket, head, localIP, serverObj.port);
-            }, (e) => {
-                console.error(e);
-            });
-        } else {
-            connect(req, cltSocket, head, srvUrl.hostname, srvUrl.port);
+    if (typeof sslConnectInterceptor === "function" && sslConnectInterceptor.call(null, req, cltSocket, head)) {
+      fakeServerCenter.getServerPromise(srvUrl.hostname, srvUrl.port).then(
+        serverObj => {
+          connect(req, cltSocket, head, localIP, serverObj.port);
+        },
+        e => {
+          console.error(e);
         }
-    };
+      );
+    } else {
+      connect(req, cltSocket, head, srvUrl.hostname, srvUrl.port);
+    }
+  };
 };
 
-function connect (req, cltSocket, head, hostname, port) {
-    // tunneling https
-    var proxySocket = net.connect(port, hostname, () => {
-        cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-        'Proxy-agent: scraperapi\r\n' +
-        '\r\n');
-        proxySocket.write(head);
-        proxySocket.pipe(cltSocket);
-        cltSocket.pipe(proxySocket);
-    });
-    proxySocket.on('error', (e) => {
-        console.log(colors.red(e));
-    });
-    proxySocket.on('ready', () => {
-        proxySocket.connectKey = `${proxySocket.localPort}:${proxySocket.remotePort}`;
-        connections[proxySocket.connectKey] = req;
-    });
-    proxySocket.on('end', () => {
-        delete connections[proxySocket.connectKey];
-    });
+function connect(req, cltSocket, head, hostname, port) {
+  // tunneling https
+  var proxySocket = net.connect(port, hostname, () => {
+    cltSocket.write("HTTP/1.1 200 Connection Established\r\n" + "Proxy-agent: scraperapi\r\n" + "\r\n");
+    proxySocket.write(head);
+    proxySocket.pipe(cltSocket);
+    cltSocket.pipe(proxySocket);
+  });
+  proxySocket.on("error", e => {
+    console.log(colors.red(e));
+  });
+  proxySocket.on("ready", () => {
+    proxySocket.connectKey = `${proxySocket.localPort}:${proxySocket.remotePort}`;
+    connections[proxySocket.connectKey] = req;
+  });
+  proxySocket.on("end", () => {
+    delete connections[proxySocket.connectKey];
+  });
 
-    return proxySocket;
+  return proxySocket;
 }
--- src/mitmproxy/middlewareHandler.js
+++ src/mitmproxy/middlewareHandler.js
@@ -1,22 +1,19 @@
-var _ = require('lodash');
-module.exports = (middlewares) => {
-    if (middlewares) {
-        if (Object.prototype.toString.call(middlewares) !== '[object Array]') {
-            throw new TypeError('middlewares must be a array');
-        }
+var _ = require("lodash");
+module.exports = middlewares => {
+  if (middlewares) {
+    if (Object.prototype.toString.call(middlewares) !== "[object Array]") {
+      throw new TypeError("middlewares must be a array");
     }
+  }
 
-    var sslConnectInterceptors = [];
-    var requestInterceptors = [];
-    var responseInterceptors = [];
+  var sslConnectInterceptors = [];
+  var requestInterceptors = [];
+  var responseInterceptors = [];
 
-    _.each(middlewares, (m) => {
-        if (m.buildIn === false || m.buildIn === 'false') {
-
-        } else {
-            m.name
-        }
-    })
-
-
-}
+  _.each(middlewares, m => {
+    if (m.buildIn === false || m.buildIn === "false") {
+    } else {
+      m.name;
+    }
+  });
+};
